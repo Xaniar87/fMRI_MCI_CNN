4cd7832be37890c67e22c96cd7d2e7d9f07ba982
Xaniar87 <zaniar.ardalan@gmail.com>
2019-02-09 16:55:50 -0700

    Update cnn_2layer.py

--- cnn_2layer.py
+++ cnn_2layer.py
@@ -7,7 +7,7 @@ def cnn_2layer (n_inputs, n_outputs, height, width, channel):
 	conv2 = tf.layers.conv2d(conv1, filters=64, kernel_size=3, strides=1, padding="SAME", activation=tf.nn.relu, name="conv2")
 
 	pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
-	pool2_flat = tf.reshape(pool2, shape=[-1,64*50*50])
+	pool2_flat = tf.reshape(pool2, shape=[-1,64*64*64])
 
 	fc1 = tf.layers.dense(pool2_flat, 64, activation = tf.nn.relu,name = "fc1")
 	logits = tf.layers.dense(fc1, n_outputs, name = "output")
